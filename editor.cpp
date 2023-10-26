7174dae2d48f8f5c15ba8de03cd0aea29d32aa79
Andy <shinjo1233@gmail.com>
2023-10-26 13:41:12 -0700

    add bullet point to text implemented

--- editor.cpp
+++ editor.cpp
@@ -2,51 +2,95 @@
 
 Editor::Editor(QWidget *parent) : QTextEdit(parent)
 {
-    QTextListFormat list_format;
-    list_format.setStyle(QTextListFormat::ListDisc);
-    this->textCursor().insertList(list_format);
+    QTextBlockFormat blockFormat = textCursor().blockFormat();
+    blockFormat.setLeftMargin(10);
+    textCursor().setBlockFormat(blockFormat);
 }
 
 void Editor::keyPressEvent(QKeyEvent *event)
 {
     int keyPressed = event->key();
+    bool cursorOnList = this->textCursor().currentList() != nullptr;
 
-    if((keyPressed == Qt::Key_Return || keyPressed == Qt::Key_Enter))
+    if(!cursorOnList)
     {
-        qDebug() << "Enter or return key pressed.";
-
-        if(isLineEmpty(this->textCursor()))
+        if(keyPressed == Qt::Key_Space)
         {
-            this->textCursor().insertText("\n");
-        }
-        else if(!isLineEmpty(this->textCursor()))
-        {
-            QTextEdit::keyPressEvent(event);
-        }
-    }
-    else if(keyPressed == Qt::Key_Backspace)
-    {
-        if(isCursorAtStartOfLine(this->textCursor()))
-        {
-            qDebug() << "Cursor is at the start of a line.";
+            qDebug() << "Spaced pressed.";
+            QString text = this->textCursor().block().text();
 
-            QString currentBlockText = this->textCursor().block().text();
-            deleteLine();
-            QTextEdit::keyPressEvent(event);
-            this->textCursor().deletePreviousChar();
-            // gotoPreviousLine();
-            this->textCursor().insertText(currentBlockText);
-
-
+            if(text.compare("-") == 0)
+            {
+                QTextBlockFormat blockFormat = textCursor().blockFormat();
+                blockFormat.setLeftMargin(20);
+                textCursor().setBlockFormat(blockFormat);
+                deleteLine();
+                QTextListFormat listFormat;
+                listFormat.setStyle(QTextListFormat::ListDisc);
+                listFormat.setIndent(0);
+                this->textCursor().insertList(listFormat);
+            }
         }
         else
         {
             QTextEdit::keyPressEvent(event);
         }
     }
-    else
+    else if(cursorOnList)    // cursor is on a bullet point
     {
-        QTextEdit::keyPressEvent(event);
+        if(keyPressed == Qt::Key_Return || keyPressed == Qt::Key_Enter)
+        {
+            qDebug() << "Enter or return key pressed.";
+
+            if(isLineEmpty(this->textCursor()))
+            {
+                this->textCursor().insertText("\n");
+            }
+            else if(!isLineEmpty(this->textCursor()))
+            {
+                QTextEdit::keyPressEvent(event);
+            }
+        }
+        else if(keyPressed == Qt::Key_Backspace)
+        {
+            if(isCursorAtStartOfLine(this->textCursor()))
+            {
+                qDebug() << "Cursor is at the start of a line.";
+
+                // Case: 1 bullet point
+                if(this->textCursor().currentList()->count() == 1)
+                {
+                    QTextEdit::keyPressEvent(event);
+                }
+
+                QString currentBlockText = this->textCursor().block().text();
+
+                deleteLine();
+
+                QTextCursor temp = this->textCursor();
+                temp.movePosition(QTextCursor::Up);
+
+                if(!isLineEmpty(temp))
+                {
+                    QTextEdit::keyPressEvent(event);
+                }
+
+                this->textCursor().deletePreviousChar();
+                this->textCursor().insertText(currentBlockText);
+            }
+            else
+            {
+                QTextEdit::keyPressEvent(event);
+            }
+        }
+        else if(keyPressed == Qt::Key_Tab)
+        {
+            qDebug() << "Tab pressed.";
+        }
+        else
+        {
+            QTextEdit::keyPressEvent(event);
+        }
     }
 }
 
